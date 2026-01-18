2584a1274bb3ab1de0de1157a48e7bf802f67feb
mero ai sathi <meroaisathi@gmail.com>
2026-01-18 22:05:08 +0545

    Fixed vite config base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Program-Management-System/", // यो लाइन अनिवार्य छ (तपाईंको रिपोजिटरीको नाम)
+  // GitHub Repo को नाम ठ्याक्कै यहाँ राख्नुहोला (Case Sensitive हुन्छ)
+  base: "/Program-Management-System/",
 })
